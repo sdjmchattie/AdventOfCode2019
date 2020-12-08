1a11934e46b1219c2e99d7b238e273819f9963e5
Stuart McHattie <stuart.mchattie@gmail.com>
2020-12-08 23:16:48 +0000

    Add solution for day 5 part 1

--- day_05/day05.py
+++ day_05/day05.py
@@ -6,31 +6,13 @@ f.close()
 
 orig_int_code = list(map((lambda x: int(x)), cs_int_code.split(',')))
 int_code_input = list(orig_int_code)
-int_code_input[1] = 12
-int_code_input[2] = 2
 int_code = IntCode(int_code_input)
+int_code.input = 1
 int_code.run()
 
 print('Part 1')
-print('  After running, position 0 contains: {}'.format(int_code.result_code[0]))
-
-
-def find_noun_verb(input_code, target_output):
-    for noun_candidate in range(100):
-        for verb_candidate in range(100):
-            new_int_code = list(input_code)
-            new_int_code[1] = noun_candidate
-            new_int_code[2] = verb_candidate
-            int_code_machine = IntCode(new_int_code)
-            int_code_machine.run()
-
-            if int_code_machine.result_code is not None and \
-                    int_code_machine.result_code[0] == target_output:
-                return noun_candidate, verb_candidate
-
-
-(noun, verb) = find_noun_verb(list(orig_int_code), 19690720)
+print('  After running, the output is: {}'.format(int_code.output))
 
 print()
 print('Part 2')
-print('  Input noun is {} and verb is {}'.format(noun, verb))
+print('  Input noun is {} and verb is {}')
--- lib/int_code.py
+++ lib/int_code.py
@@ -6,30 +6,55 @@ class ValueMode(Enum):
     IMMEDIATE = 1
 
 
+def parse_op_code(code):
+    code_string = str(code)
+    op_code = int(code_string[-2:])
+    code_string = code_string[:-2]
+
+    value_modes = []
+    while len(code_string) > 0:
+        value_modes.append(ValueMode(int(code_string[-1:])))
+        code_string = code_string[:-1]
+
+    while len(value_modes) < 4:
+        value_modes.append(ValueMode.POSITION)
+
+    return op_code, value_modes
+
+
 class IntCode:
     def __init__(self, code):
         self.initial_code = code
         self.code = None
+        self.input = None
+        self.output = None
 
     def run(self):
         pos = 0
         self.code = list(self.initial_code)
+        self.output = None
 
         try:
             while self.code[pos] != 99:
-                op_code = self.get_value(pos, ValueMode.IMMEDIATE)
+                (op_code, value_modes) = parse_op_code(self.get_value(pos, ValueMode.IMMEDIATE))
                 if op_code == 1:
-                    self.add(pos)
+                    self.add(pos + 1, value_modes)
                     pos += 4
                 elif op_code == 2:
-                    self.multiply(pos)
+                    self.multiply(pos + 1, value_modes)
                     pos += 4
+                elif op_code == 3:
+                    self.set_value(pos + 1, self.input, value_modes[0])
+                    pos += 2
+                elif op_code == 4:
+                    self.output = self.get_value(pos + 1, value_modes[0])
+                    pos += 2
                 else:
                     raise ValueError('Operator code {} is not recognised.'.format(op_code))
         except IndexError:
             pass
 
-    def get_value(self, position, mode=ValueMode.POSITION):
+    def get_value(self, position, mode):
         if mode == ValueMode.POSITION:
             return self.code[self.code[position]]
         elif mode == ValueMode.IMMEDIATE:
@@ -37,7 +62,7 @@ class IntCode:
         else:
             raise ValueError('Value Mode {} is not recognised.'.format(mode))
 
-    def set_value(self, position, new_value, mode=ValueMode.POSITION):
+    def set_value(self, position, new_value, mode):
         if mode == ValueMode.POSITION:
             self.code[self.code[position]] = new_value
         elif mode == ValueMode.IMMEDIATE:
@@ -45,8 +70,10 @@ class IntCode:
         else:
             raise ValueError('Value Mode {} is not recognised.'.format(mode))
 
-    def add(self, position):
-        self.set_value(position + 3, self.get_value(position + 1) + self.get_value(position + 2))
+    def add(self, position, value_modes):
+        op_result = self.get_value(position, value_modes[0]) + self.get_value(position + 1, value_modes[1])
+        self.set_value(position + 2, op_result, value_modes[2])
 
-    def multiply(self, position):
-        self.set_value(position + 3, self.get_value(position + 1) * self.get_value(position + 2))
+    def multiply(self, position, value_modes):
+        op_result = self.get_value(position, value_modes[0]) * self.get_value(position + 1, value_modes[1])
+        self.set_value(position + 2, op_result, value_modes[2])
