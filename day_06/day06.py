5b55dea3bb148c5364739e72859a13745fbf2f6c
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-06 21:21:03 +0100

    Complete part 2 of day 6

--- day_06/day06.py
+++ day_06/day06.py
@@ -3,7 +3,8 @@ from typing import NewType
 
 
 class OrbitalBody:
-    def __init__(self) -> None:
+    def __init__(self, id) -> None:
+        self.id = id
         self.parent = None
 
     def parental_orbits(self, accum=0):
@@ -12,12 +13,18 @@ class OrbitalBody:
         else:
             return self.parent.parental_orbits(accum) + 1
 
+    def ancestors(self, accum=[]):
+        if self.parent is None:
+            return accum
+        else:
+            return [self.parent.id] + self.parent.ancestors()
+
 
 def get_or_create_body(bodies, id):
     try:
         return bodies[id]
     except KeyError:
-        new_body = OrbitalBody()
+        new_body = OrbitalBody(id)
         bodies[id] = new_body
         return new_body
 
@@ -46,6 +53,10 @@ print("Part 1")
 print(f"  Number of orbits: {total_orbits}")
 
 
+me_ancestors = bodies['YOU'].ancestors()
+santa_ancestors = bodies['SAN'].ancestors()
+common_ancestor = next(i for i in santa_ancestors if i in me_ancestors)
+
 print()
 print("Part 2")
-print(f"  Answer for part 2: {0}")
+print(f"  Answer for part 2: {me_ancestors.index(common_ancestor) + santa_ancestors.index(common_ancestor)}")
