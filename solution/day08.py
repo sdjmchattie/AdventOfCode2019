403aec3046c24550a5f12a1ba75e385e526d0571
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-07 16:42:37 +0100

    Provide solution for part 1 of day 8

--- solution/day08.py
+++ solution/day08.py
@@ -1,13 +1,19 @@
 IMAGE_WIDTH = 25
 IMAGE_HEIGHT = 6
+LAYER_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
 
 def solve():
   f = open("input/day08.txt", "r")
-  pixel_data = f.read()
+  pixel_data = f.read().strip('\n')
   f.close()
 
+  layer_count = int(len(pixel_data) / LAYER_SIZE)
+  layer_starts = [i * LAYER_SIZE for i in range(layer_count)]
+  layers_data = [pixel_data[i:i+LAYER_SIZE] for i in layer_starts]
+  check_layer = min(layers_data, key = lambda x: x.count('0'))
+
   print("Part 1")
-  print(f"  1s and 2s product: {0}")
+  print(f"  1s and 2s product: {check_layer.count('1') * check_layer.count('2')}")
 
   print()
   print("Part 2")
