ca2c3731e5ec4fa8fa213844f8b3e95afee4d3fc
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-07 16:27:14 +0100

    Prep day 8 module

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from solution import (
     day01, day02, day03, day04, day05,
-    day06, day07,
+    day06, day07, day08
 )
 
 if __name__ == '__main__':
-    day07.solve()
+    day08.solve()
---
+++ solution/day08.py
@@ -0,0 +1,14 @@
+IMAGE_WIDTH = 25
+IMAGE_HEIGHT = 6
+
+def solve():
+  f = open("input/day08.txt", "r")
+  pixel_data = f.read()
+  f.close()
+
+  print("Part 1")
+  print(f"  1s and 2s product: {0}")
+
+  print()
+  print("Part 2")
+  print(f"  Part 2: {0}")
