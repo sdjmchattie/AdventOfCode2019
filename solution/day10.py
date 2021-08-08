5b0f25c6fc41e9282167c57902b5272127732f10
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-08 14:00:34 +0100

    Add solution for part 1 of day 10

--- solution/day10.py
+++ solution/day10.py
@@ -16,24 +16,24 @@ def radial_angle(dx, dy):
 
 
 def solve():
-  # f = open("input/day09.txt", "r")
-  # raw_code = f.read().strip('\n')
-  # f.close()
+  f = open("input/day10.txt", "r")
+  raw_map = f.readlines()
+  f.close()
 
-  map = [
-    '......#.#.',
-    '#..#.#....',
-    '..#######.',
-    '.#.#.###..',
-    '.#..#.....',
-    '..#....#.#',
-    '#..#....#.',
-    '.##.#..###',
-    '##...#..#.',
-    '.#....####',
-  ]
+  asteroids = []
+  for y, row in enumerate(raw_map):
+    for x, marker in enumerate(row):
+      if marker == '#':
+        asteroids.append([x, y])
+
+  best = 0
+  for asteroid in asteroids:
+    angles = [radial_angle(a[0] - asteroid[0], a[1] - asteroid[1]) for a in asteroids if a != asteroid]
+    score = len(set(angles))
+    best = max(best, score)
 
   print("Part 1")
+  print(f'  Best visibility is {best}')
 
   print()
   print("Part 2")
