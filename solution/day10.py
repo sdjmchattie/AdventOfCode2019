8841175d548165680095c3a17e874eb14f9379ac
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-08 14:22:51 +0100

    Add solution for part 2 to day 10

--- solution/day10.py
+++ solution/day10.py
@@ -26,14 +26,38 @@ def solve():
       if marker == '#':
         asteroids.append([x, y])
 
-  best = 0
+  best_visibility = 0
+  best_location = None
   for asteroid in asteroids:
     angles = [radial_angle(a[0] - asteroid[0], a[1] - asteroid[1]) for a in asteroids if a != asteroid]
     score = len(set(angles))
-    best = max(best, score)
+    if best_visibility < score:
+      best_visibility = score
+      best_location = asteroid
 
   print("Part 1")
-  print(f'  Best visibility is {best}')
+  print(f'  Best visibility is {best_visibility}')
+  print(f'  Best location is {best_location}')
+
+  # Get the angle of each asteroid from the best location
+  asteroids_at_angle = {}
+  for asteroid in asteroids:
+    if asteroid == best_location:
+      continue
+
+    angle = radial_angle(asteroid[0] - best_location[0], asteroid[1] - best_location[1])
+    inline_asteroids = asteroids_at_angle.get(angle, [])
+    inline_asteroids.append(asteroid)
+    asteroids_at_angle[angle] = inline_asteroids
+
+  # Find the 200th angle and then the nearest asteroid at that angle
+  angle_200 = sorted(list(asteroids_at_angle.keys()))[199]
+
+  def distance(asteroid):
+    math.sqrt(math.pow(asteroid[0] - best_location[0], 2) + math.pow(asteroid[1] - best_location[1], 2))
+
+  closest_asteroid = min(asteroids_at_angle[angle_200], key = distance)
 
   print()
   print("Part 2")
+  print(f'  200th asteroid to be vaporised is at {closest_asteroid}')
