0d6724ecf0e71738f5cc17398a9cd1af3ff19249
Stuart McHattie <stuart.mchattie@gmail.com>
2021-08-31 15:10:40 +0100

    Setup day 14

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from solution import (
     day01, day02, day03, day04, day05,
     day06, day07, day08, day09, day10,
-    day11, day12, day13
+    day11, day12, day13, day14
 )
 
 if __name__ == '__main__':
-    day13.solve()
+    day14.solve()
---
+++ solution/day14.py
@@ -0,0 +1,12 @@
+def solve():
+  f = open("input/day14.txt", "r")
+  recipes = f.readlines()
+  f.close()
+
+  print("Part 1")
+  print(f'  Answer = {0}')
+
+  print()
+  print("Part 2")
+  print(f'  Answer = {0}')
+  print()
